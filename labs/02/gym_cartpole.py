1f21b7ef615d0d8df7d72bbaf6557b13170c15b9
Peter Polak <polakpe7@gmail.com>
2019-03-17 17:29:59 +0100

    cartpole homework

--- labs/02/gym_cartpole.py
+++ labs/02/gym_cartpole.py
@@ -10,8 +10,8 @@ import tensorflow as tf
 # Parse arguments
 # TODO: Set reasonable defaults and possibly add more arguments.
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
+parser.add_argument("--batch_size", default=1, type=int, help="Batch size.")
+parser.add_argument("--epochs", default=80, type=int, help="Number of epochs.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 args = parser.parse_args()
 
@@ -41,7 +41,12 @@ observations, labels = np.array(observations), np.array(labels)
 # However, beware that there is currently a bug in Keras which does
 # not correctly serialize InputLayer. Instead of using an InputLayer,
 # pass explicitly `input_shape` to the first real model layer.
-model = None
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=[4,]),
+    tf.keras.layers.Dense(4, activation=tf.nn.relu),
+    tf.keras.layers.Dense(2, activation=tf.nn.relu),
+])
+
 
 model.compile(
     optimizer=tf.keras.optimizers.Adam(),
