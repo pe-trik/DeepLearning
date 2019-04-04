d8c21431820f1fd062896ab8a1371761ae7b94d2
Your Name <you@example.com>
2019-04-04 18:23:24 +0200

    uppercase

--- labs/03/uppercase.py
+++ labs/03/uppercase.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
+#
+# All team solutions **must** list **all** members of the team.
+# The members must be listed using their ReCodEx IDs anywhere
+# in a comment block in the source file (on a line beginning with `#`).
+#
+# You can find out ReCodEx ID in the URL bar after navigating
+# to your User profile page. The ID has the following format:
+# 55d46f86-b962-11e7-a937-00505601122b
+# 4fc059fa-abd2-11e7-a937-00505601122b
 import argparse
 import datetime
 import os
 import re
-
+import random
 import numpy as np
 import tensorflow as tf
 
@@ -11,12 +20,15 @@ from uppercase_data import UppercaseData
 
 # Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--alphabet_size", default=None, type=int, help="If nonzero, limit alphabet to this many most frequent chars.")
-parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=10, type=int, help="Number of epochs.")
-parser.add_argument("--hidden_layers", default="500", type=str, help="Hidden layer configuration.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-parser.add_argument("--window", default=None, type=int, help="Window size to use.")
+parser.add_argument("--alphabet_size", default=48, type=int, help="If nonzero, limit alphabet to this many most frequent chars.")
+parser.add_argument("--batch_size", default=512, type=int, help="Batch size.")
+parser.add_argument("--dropout", default=0.3, type=float, help="Dropout regularization.")
+parser.add_argument("--l2", default=0.1, type=float, help="L2 regularization.")
+parser.add_argument("--epochs", default=5, type=int, help="Number of epochs.")
+parser.add_argument("--hidden_layers", default="10,10,10", type=str, help="Hidden layer configuration.")
+#parser.add_argument("--hidden_layer_neurons", default="100", type=str, help="Hidden layer configuration.")
+parser.add_argument("--threads", default=8, type=int, help="Maximum number of threads to use.")
+parser.add_argument("--window", default=10, type=int, help="Window size to use.")
 args = parser.parse_args()
 args.hidden_layers = [int(hidden_layer) for hidden_layer in args.hidden_layers.split(",") if hidden_layer]
 
@@ -35,6 +47,79 @@ args.logdir = os.path.join("logs", "{}-{}-{}".format(
 
 # Load data
 uppercase_data = UppercaseData(args.window, args.alphabet_size)
+
+indices_with_ones = list(np.concatenate(np.where(uppercase_data.train.data["labels"] == 1)))
+zeros = list(np.concatenate(np.where(uppercase_data.train.data["labels"] == 0)))
+indices_with_zeros = list(zeros) #random.sample(zeros,len(indices_with_ones)*3)
+print(len(indices_with_ones))
+print(len(indices_with_zeros))
+result_indices = indices_with_ones+indices_with_zeros#+indices_with_ones
+print(len(result_indices))
+train_data = dict()
+train_data["labels"] = uppercase_data.train.data["labels"]#[result_indices]
+train_data["windows"] = uppercase_data.train.data["windows"]#[result_indices]
+
+if args.l2 != 0:
+    regularization = tf.keras.regularizers.L1L2(l2=args.l2)
+else:
+    regularization = None
+
+args.learning_rate = 0.1
+    
+steps =  (len(train_data["labels"])*args.epochs)/args.batch_size
+exp_decay = (0.001/args.learning_rate)
+learn_rate = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate = args.learning_rate,
+    decay_steps=steps,
+    decay_rate=exp_decay
+)
+
+# Create the model
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.InputLayer(input_shape=[2 * args.window + 1], dtype=tf.int32))
+model.add(tf.keras.layers.Lambda(lambda x: tf.one_hot(x, len(uppercase_data.train.alphabet))))
+model.add(tf.keras.layers.Flatten())
+
+model.add(tf.keras.layers.Dense(1050, 
+                                    activation=tf.nn.relu
+                                    ))
+
+model.add(tf.keras.layers.Dropout(rate=args.dropout))
+
+model.add(tf.keras.layers.Dense(1050, 
+                                    activation=tf.nn.tanh
+                                    ))
+
+model.add(tf.keras.layers.Dropout(rate=args.dropout))
+
+model.add(tf.keras.layers.Dense(42, 
+                                    activation=tf.nn.relu
+                                    ))
+model.add(tf.keras.layers.Dropout(rate=args.dropout))
+
+
+model.add(tf.keras.layers.Dense(2, activation=tf.nn.softmax)
+          )
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+)
+
+tb_callback=tf.keras.callbacks.TensorBoard(args.logdir, histogram_freq=1000000, update_freq=1000000, profile_batch=1)
+tb_callback.on_train_end = lambda *_: None
+model.fit(
+    train_data["windows"], train_data["labels"],
+    batch_size=args.batch_size, epochs=args.epochs,
+    validation_data=(uppercase_data.dev.data["windows"], uppercase_data.dev.data["labels"]),
+    callbacks=[tb_callback],
+)
+
+#test_logs = model.evaluate(
+#    uppercase_data.test.data["images"], mnist.test.data["labels"], batch_size=args.batch_size,
+#)
+#tb_callback.on_epoch_end(1, dict(("val_test_" + metric, value) for metric, value in zip(model.metrics_names, test_logs)))
 
 # TODO: Implement a suitable model, optionally including regularization, select
 # good hyperparameters and train the model.
@@ -55,9 +140,17 @@ uppercase_data = UppercaseData(args.window, args.alphabet_size)
 #   You can then flatten the one-hot encoded windows and follow with a dense layer.
 # - Alternatively, you can use `tf.keras.layers.Embedding`, which is an efficient
 #   implementation of one-hot encoding followed by a Dense layer, and flatten afterwards.
-
-with open("uppercase_test.txt", "w") as out_file:
+import io
+with io.open("uppercase_test.txt", "w", encoding="utf-8") as out_file:
+    results = np.argmax(model.predict(uppercase_data.test.data["windows"]), axis=1)
+    for i in range(len(results)):
+        ch = uppercase_data.test.text[i]
+        if results[i]==1:
+            ch = ch.upper()
+        out_file.write(ch)
     # TODO: Generate correctly capitalized test set.
     # Use `uppercase_data.test.text` as input, capitalize suitable characters,
-    # and write the result to `uppercase_test.txt` file.
+    #    # and write the result to `uppercase_test.txt` file.
+
     pass
+
